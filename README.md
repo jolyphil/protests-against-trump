a096de6238651b8281ca4997250cbdbb85b70356
jolyphil <philippe.joly@posteo.net>
2025-07-07 13:47:50 +0200

    First draft, full

--- README.md
+++ README.md
@@ -1,33 +1,55 @@
-Protests against Trump
+Protests against Trump, January 1st, 2025 to May 31st, 2025
 ================
 Philippe Joly
-2025-07-06
+2025-07-07
 
 This project uses data from the Crowd Counting Consortium (CCC) to
 analyze protests against Trump in 2025.
 
-``` r
-library(dplyr)
-library(ggplot2)
-library(ggrepel)
-```
+- The outcome variable is the number of participants per 1,000
+  inhabitants from January 1st, 2025 to May 31st, 2025.
 
-``` r
-ccc <- readRDS("data/ccc_states_merged.rds")
-```
+## Bivariate Relationships
 
-## Support for Trump
-
-``` r
-ccc |> 
-  filter(!is.na(pct_trump), !is.na(n_part_pop), state != "DC") |> 
-  ggplot(aes(x = pct_trump, y = n_part_pop)) +
-  geom_point() +
-  geom_smooth(method = "lm", formula = "y ~ x") + 
-  geom_text_repel(aes(label = state)) +
-  labs(x = "Support for Trump (%), 2024 presidential election",
-       y = "Number of participants per thousand inhabitants") +
-  theme_minimal()
-```
+### Support for Trump (%), 2024 presidential election
 
 ![](README_files/figure-gfm/trump-support-1.png)<!-- -->
+
+### Median Annual Household Income
+
+![](README_files/figure-gfm/income-1.png)<!-- -->
+
+### Percentage of Black Residents
+
+![](README_files/figure-gfm/black-1.png)<!-- -->
+
+### Percentage of Hispanic Residents
+
+![](README_files/figure-gfm/hispanic-1.png)<!-- -->
+
+## Percentage of Noncitizen Residents
+
+![](README_files/figure-gfm/non-citizens-1.png)<!-- -->
+
+## Governor Party
+
+![](README_files/figure-gfm/gov-party-1.png)<!-- -->
+
+## Correlation Matrix
+
+|                | n_part_pop | pct_trump | income | black | hispanic | non_citizens | republican_gov |
+|----------------|------------|-----------|--------|-------|----------|--------------|----------------|
+| n_part_pop     | 1          | .         | .      | .     | .        | .            | .              |
+| pct_trump      | -.47       | 1         | .      | .     | .        | .            | .              |
+| income         | .33        | -.71      | 1      | .     | .        | .            | .              |
+| black          | -.48       | .02       | -.25   | 1     | .        | .            | .              |
+| hispanic       | -.10       | -.29      | .24    | -.12  | 1        | .            | .              |
+| non_citizens   | -.05       | -.63      | .60    | .07   | .76      | 1            | .              |
+| republican_gov | -.18       | .66       | -.41   | .11   | -.29     | -.41         | 1              |
+
+## Multivariate Analysis
+
+Results of a count model. Coefficients below zero indicate a negative
+effect.
+
+![](README_files/figure-gfm/multivariate-1.png)<!-- -->
--- data_raw/data_preparation.R
+++ data_raw/data_preparation.R
@@ -1,4 +1,3 @@
-library(countrycode)
 library(dplyr)
 library(readr)
 library(rvest)
@@ -83,6 +82,59 @@ kff_pop <- read_csv("data_raw/KFF/kff_total_number_of_residents.csv",
                            TRUE ~ state.abb[match(state, state.name)]),
          pop = pop / 1000000)
 
+kff_income <- read_csv("data_raw/KFF/kff_median_annual_household_income.csv",
+                       skip = 4,
+                       n_max = 52,
+                       col_names = c("state", "income")) |> 
+  mutate(state = case_when(state == "District of Columbia" ~ "DC",
+                           state == "Puerto Rico" ~ "PR",
+                           TRUE ~ state.abb[match(state, state.name)]),
+         income = str_remove(income, "\\$") |> as.numeric(),
+         income = income / 1000)
+
+
+kff_ethnicity <- read_csv("data_raw/KFF/kff_population_distribution_by_race_ethnicity.csv",
+                          skip = 4,
+                          n_max = 52,
+                          col_names = FALSE
+                       ) |> 
+  select(state = X1, 
+         black = X3, 
+         hispanic = X4) |> 
+  mutate(state = case_when(state == "District of Columbia" ~ "DC",
+                           state == "Puerto Rico" ~ "PR",
+                           TRUE ~ state.abb[match(state, state.name)]),
+         black = black * 100,
+         hispanic = hispanic * 100)
+
+
+kff_citizenship <- read_csv("data_raw/KFF/kff_population_distribution_by_citizenship_status.csv",
+                            skip = 4,
+                            n_max = 52,
+                            col_names = FALSE) |> 
+  mutate(
+    state = case_when(X1 == "District of Columbia" ~ "DC",
+                      X1 == "Puerto Rico" ~ "PR",
+                      TRUE ~ state.abb[match(X1, state.name)]),
+    across(c(X4, X5, X5), as.numeric),
+    pct_non_citizens = ((X4 + X5) / X6) * 100
+  ) |> 
+  select(state, pct_non_citizens)
+
+
+kff_gov_party <- read_csv("data_raw/KFF/kff_state_political_parties.csv",
+                          skip = 4,
+                          n_max = 52,
+                          col_names = FALSE) |> 
+  select(state = X1,
+         gov_party = X2) |> 
+  mutate(
+    state = case_when(state == "District of Columbia" ~ "DC",
+                      state == "Puerto Rico" ~ "PR",
+                      TRUE ~ state.abb[match(state, state.name)]),
+    gov_party = factor(gov_party)
+  )
+
 
 # Join --------------------------------------------------------------------
 
@@ -90,11 +142,29 @@ ccc_states_merged <- ccc_states |>
   left_join(kff_pop, by = "state") |> 
   mutate(n_events_pop = n_events/pop,
          n_events_pop_log = log(n_events_pop),
-         n_part_pop = n_part/(pop * 1000)) |> 
-  left_join(app, by = "state") 
+         n_part_pop = n_part/(pop * 1000),
+         n_part_pop = round(n_part_pop) # for the count models
+         ) |> 
+  left_join(app, by = "state") |> 
+  left_join(kff_income, by = "state") |> 
+  left_join(kff_ethnicity, by = "state") |> 
+  left_join(kff_citizenship, by = "state") |> 
+  left_join(kff_gov_party, by = "state")
 
 
 # Save --------------------------------------------------------------------
 
 saveRDS(ccc_states_merged, file = "data/ccc_states_merged.rds")
 
+
+# Export subsample --------------------------------------------------------
+
+set.seed(123)
+
+ccc_sub <- ccc_against_trump |>
+  filter(!is.na(claims_verbatim) & !is.na(title)) |> 
+  mutate(percentile = percent_rank(size_mean)) |>       # 1. Compute percentiles (0 to 1)
+  filter(percentile >= 0.9) |>                         # 2. Keep top 10%
+  slice_sample(n = 50)                                  # 3. Random sample of 50 rows
+
+write_csv(ccc_sub, file = "ccc_sub.csv")
